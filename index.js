ce38626589fcc4a30b76d8316d9e62a9d4e37bf8
Alejoml07 <alejandromunozlezcano@gmail.com>
2022-07-29 17:29:32 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/",(req, res) => {
+    res.send("Hola")
+})
+
+app.listen(8070,()=> {
+    console.log("Servidor fucionando")
+})
